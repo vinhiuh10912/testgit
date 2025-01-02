b6097a5cd6e63548ffd70c2d00d9bf0663412261
vinhiuh10912 <nguyentanvinh1912@gmail.com>
2025-01-02 13:00:30 +0700

    bai1_php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta content="charset=utf-8" />
+        <title>bai 1</title>
+    </head>
+    <body>
+        <?php 
+        echo 'Hello PHP!';
+        ?>
+    </body>
+</html>
